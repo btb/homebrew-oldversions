3ea3335eea5b58b7cd7da9cb31c44785274fa09c
Bradley Bell <bradley.bell@gmail.com>
2021-07-15 17:09:32 -0700

    ruby fixes

--- Formula/physfs@2.0.3.rb
+++ Formula/physfs@2.0.3.rb
@@ -3,7 +3,7 @@ class PhysfsAT203 < Formula
   homepage "https://icculus.org/physfs/"
   url "https://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2"
   sha256 "ca862097c0fb451f2cacd286194d071289342c107b6fe69079c079883ff66b69"
-  head "https://hg.icculus.org/icculus/physfs/", :using => :hg
+  head "https://hg.icculus.org/icculus/physfs/", using: :hg
 
   depends_on "cmake" => :build
 
@@ -23,7 +23,7 @@ class PhysfsAT203 < Formula
     (testpath/"test").write <<~EOS
       addarchive test.zip 1
       cat test.txt
-      EOS
-    assert_match /Successful\.\nhomebrew/, shell_output("#{bin}/test_physfs < test 2>&1")
+    EOS
+    assert_match(/Successful\.\nhomebrew/, shell_output("#{bin}/test_physfs < test 2>&1"))
   end
 end
